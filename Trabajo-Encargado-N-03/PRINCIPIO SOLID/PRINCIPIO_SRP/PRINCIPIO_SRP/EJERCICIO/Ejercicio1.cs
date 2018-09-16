54c67b1a87c1eee0b99e03a1aacd67551a243b50
LeydiHuallpa <leydihuallpa67@gmail.com>
2018-09-16 06:39:40 -0500

    primer commit
    este es el primer commit de la tarea

---
+++ Trabajo-Encargado-N-03/PRINCIPIO SOLID/PRINCIPIO_SRP/PRINCIPIO_SRP/EJERCICIO/Ejercicio1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRINCIPIO_SRP.EJERCICIO
+{
+    public class Ejercicio1
+    {
+        public double Sides { get; } = 4;
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        //public class AreaOperations
+        //{
+        //    internal static object SumAreas(IEnumerable<Ejercicio1> rectangulos)
+        //    {
+        //        double area = 0;
+        //        foreach (var rectangle in rectangulos)
+        //            area += rectangle.Height * rectangle.Width;
+        //        return area;
+        //    }
+        //}
+
+        internal static object SumAreas(Ejercicio1[] rectangulos)
+        {
+            double area = 0;
+            foreach (var rectangle in rectangulos)
+                area += rectangle.Height * rectangle.Width;
+            return area;
+        }
+
+        internal static object SumPerimeters(Ejercicio1[] rectangulos)
+        {
+            double perimeter = 0;
+            foreach (var rectangle in rectangulos)
+                perimeter += 2 * rectangle.Height + 2 * rectangle.Width;
+            return perimeter;
+        }
+
+        //public class PerimeterOperations
+        //{
+        //    public double SumPerimeters(IEnumerable<Ejercicio1> rectangulos)
+        //    {
+        //        double perimeter = 0;
+        //        foreach (var rectangle in rectangulos)
+        //            perimeter += 2 * rectangle.Height + 2 * rectangle.Width;
+        //        return perimeter;
+        //  }
+        //}
+    }
+}
---
+++ Trabajo-Encargado-N-03/PRINCIPIO SOLID/PRINCIPIO_SRP/PRINCIPIO_SRP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRINCIPIO_SRP
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rectangulos = new[]
+{
+    new EJERCICIO.Ejercicio1 {Width = 10, Height = 5},
+    new EJERCICIO.Ejercicio1 {Width = 4, Height = 6},
+    new EJERCICIO.Ejercicio1 {Width = 5, Height = 1},
+    new EJERCICIO.Ejercicio1 {Width = 8, Height = 9}
+};
+
+            var sumaDeAreas = EJERCICIO.Ejercicio1.SumAreas(rectangulos);
+            var sumaDePerimetros = EJERCICIO.Ejercicio1.SumPerimeters(rectangulos);
+
+            Console.WriteLine($"AREA TOTAL: {sumaDeAreas}" + 
+                                $"PERIMETRO TOTAL: {sumaDePerimetros}");
+            Console.ReadKey();
+            
+        }
+    }
+}
